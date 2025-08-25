3ec0f294f0579db4629edf32fa4a8e302c40889a
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2025-08-25 19:18:46 +0400

    Add Meta databace connections resource example

---
+++ examples/resources/superset_meta_database/import.sh
@@ -0,0 +1,2 @@
+# Database can be imported by specifying the numeric identifier of the Database id
+terraform import superset_meta_database.example 1949
---
+++ examples/resources/superset_meta_database/resource.tf
@@ -0,0 +1,14 @@
+resource "superset_meta_database" "example" {
+  database_name = "SuperSetDBConnection"
+  sqlalchemy_uri = "superset://"    # optional
+  allowed_databases = [
+    "[Team]-Service1-Dev-RO[d_team_service1_db]",
+    "[Team]-Service2-Prod-RO[d_team_market_service2_db]"
+  ]
+  expose_in_sqllab      = true
+  allow_ctas            = false
+  allow_cvas            = false
+  allow_dml             = false
+  allow_run_async       = true
+  is_managed_externally = false
+}
